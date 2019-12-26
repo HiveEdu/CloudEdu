bba26528174fd0c120258742cf87ac68a30fd3eb
king-sir <1084145369@qq.com>
2019-12-26 21:04:18 +0800

    增加APP登录注册

---
+++ myedu/src/main/java/com/myedu/project/app/controller/UserController.java
@@ -0,0 +1,82 @@
+package com.myedu.project.app.controller;
+
+import com.myedu.common.constant.UserConstants;
+import com.myedu.common.utils.SecurityUtils;
+import com.myedu.framework.web.controller.BaseController;
+import com.myedu.framework.web.domain.AjaxResult;
+import com.myedu.project.system.domain.SysUser;
+import com.myedu.project.system.service.ISysUserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: 梁少鹏
+ * Date: 2019/12/20
+ * Time: 22:30
+ * Description:
+ */
+@Api("APP用户信息管理")
+@RestController
+@RequestMapping("/app/user")
+public class UserController extends BaseController {
+
+    @Autowired
+    private ISysUserService userService;
+  /*
+   * @Description :
+   * @Author : 梁少鹏
+   * @Date : 2019/12/21 7:48
+   */
+    @ApiOperation("新增用户")
+    @ApiImplicitParam(name = "SysUser", value = "新增用户信息", dataType = "SysUser")
+    @PostMapping("/saveUser")
+    public AjaxResult saveUser(SysUser user)
+    {
+        if (UserConstants.NOT_UNIQUE.equals(userService.checkUserNameUnique(user.getUserName())))
+        {
+            return AjaxResult.error("新增用户'" + user.getUserName() + "'失败，登录账号已存在");
+        }
+        else if (UserConstants.NOT_UNIQUE.equals(userService.checkPhoneUnique(user)))
+        {
+            return AjaxResult.error("新增用户'" + user.getUserName() + "'失败，手机号码已存在");
+        }
+        else if (UserConstants.NOT_UNIQUE.equals(userService.checkEmailUnique(user)))
+        {
+            return AjaxResult.error("新增用户'" + user.getUserName() + "'失败，邮箱账号已存在");
+        }
+        user.setCreateBy(SecurityUtils.getUsername());
+        user.setPassword(SecurityUtils.encryptPassword(user.getPassword()));
+        return toAjax(userService.insertUser(user));
+    }
+
+    /*
+     * @Description :修改用户
+     * @Author : 梁少鹏
+     * @Date : 2019/12/21 7:48
+     */
+    @ApiOperation("修改用户")
+    @ApiImplicitParam(name = "SysUser", value = "修改用户信息", dataType = "SysUser")
+    @PutMapping("/editUser")
+    public AjaxResult editUser(SysUser user)
+    {
+        userService.checkUserAllowed(user);
+        if (UserConstants.NOT_UNIQUE.equals(userService.checkPhoneUnique(user)))
+        {
+            return AjaxResult.error("修改用户'" + user.getUserName() + "'失败，手机号码已存在");
+        }
+        else if (UserConstants.NOT_UNIQUE.equals(userService.checkEmailUnique(user)))
+        {
+            return AjaxResult.error("修改用户'" + user.getUserName() + "'失败，邮箱账号已存在");
+        }
+        user.setUpdateBy(SecurityUtils.getUsername());
+        return toAjax(userService.updateUser(user));
+    }
+
+}
--- myedu/src/main/java/com/myedu/project/system/domain/SysUser.java
+++ myedu/src/main/java/com/myedu/project/system/domain/SysUser.java
@@ -5,6 +5,9 @@ import java.util.List;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
 import com.myedu.framework.aspectj.lang.annotation.Excel;
@@ -18,60 +21,75 @@ import com.myedu.framework.web.domain.BaseEntity;
  * 
  * @author ruoyi
  */
+@ApiModel("用户实体")
 public class SysUser extends BaseEntity
 {
     private static final long serialVersionUID = 1L;
 
     /** 用户ID */
     @Excel(name = "用户序号", cellType = ColumnType.NUMERIC, prompt = "用户编号")
+    @ApiModelProperty("用户ID")
     private Long userId;
 
     /** 部门ID */
     @Excel(name = "部门编号", type = Type.IMPORT)
+    @ApiModelProperty("部门ID")
     private Long deptId;
 
     /** 用户账号 */
     @Excel(name = "登录名称")
+    @ApiModelProperty("用户账号")
     private String userName;
 
     /** 用户昵称 */
     @Excel(name = "用户名称")
+    @ApiModelProperty("用户昵称")
     private String nickName;
 
     /** 用户邮箱 */
     @Excel(name = "用户邮箱")
+    @ApiModelProperty("用户邮箱")
     private String email;
 
     /** 手机号码 */
     @Excel(name = "手机号码")
+    @ApiModelProperty("手机号码")
     private String phonenumber;
 
     /** 用户性别 */
     @Excel(name = "用户性别", readConverterExp = "0=男,1=女,2=未知")
+    @ApiModelProperty("用户性别")
     private String sex;
 
     /** 用户头像 */
+    @ApiModelProperty("用户头像")
     private String avatar;
 
     /** 密码 */
+    @ApiModelProperty("用户ID")
     private String password;
 
     /** 盐加密 */
+    @ApiModelProperty("盐加密")
     private String salt;
 
     /** 帐号状态（0正常 1停用） */
     @Excel(name = "帐号状态", readConverterExp = "0=正常,1=停用")
+    @ApiModelProperty("帐号状态")
     private String status;
 
     /** 删除标志（0代表存在 2代表删除） */
+    @ApiModelProperty("删除标志")
     private String delFlag;
 
     /** 最后登陆IP */
     @Excel(name = "最后登陆IP", type = Type.EXPORT)
+    @ApiModelProperty("最后登陆IP")
     private String loginIp;
 
     /** 最后登陆时间 */
     @Excel(name = "最后登陆时间", width = 30, dateFormat = "yyyy-MM-dd HH:mm:ss", type = Type.EXPORT)
+    @ApiModelProperty("最后登陆时间")
     private Date loginDate;
 
     /** 部门对象 */
@@ -79,6 +97,7 @@ public class SysUser extends BaseEntity
         @Excel(name = "部门名称", targetAttr = "deptName", type = Type.EXPORT),
         @Excel(name = "部门负责人", targetAttr = "leader", type = Type.EXPORT)
     })
+    @ApiModelProperty("部门对象")
     private SysDept dept;
 
     /** 角色对象 */
