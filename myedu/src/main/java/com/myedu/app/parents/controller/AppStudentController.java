b9646e02d7515e28c1c0aed8243e10d2cee31f4f
king-sir <1084145369@qq.com>
2020-03-27 20:41:08 +0800

    修改添加学生接口

--- myedu/src/main/java/com/myedu/app/parents/controller/AppStudentController.java
+++ myedu/src/main/java/com/myedu/app/parents/controller/AppStudentController.java
@@ -1,14 +1,21 @@
 package com.myedu.app.parents.controller;
 
+import com.myedu.common.constant.Constants;
 import com.myedu.common.utils.SecurityUtils;
+import com.myedu.common.utils.ServletUtils;
+import com.myedu.common.utils.StringUtils;
 import com.myedu.framework.aspectj.lang.annotation.Log;
 import com.myedu.framework.aspectj.lang.enums.BusinessType;
+import com.myedu.framework.redis.RedisCache;
+import com.myedu.framework.security.LoginUser;
+import com.myedu.framework.security.service.TokenService;
 import com.myedu.framework.web.controller.BaseController;
 import com.myedu.framework.web.domain.AjaxResult;
 import com.myedu.framework.web.page.TableDataInfo;
 import com.myedu.project.parents.domain.YunStudent;
 import com.myedu.project.parents.domain.vo.YunStudentVo;
 import com.myedu.project.parents.service.IYunStudentService;
+import io.jsonwebtoken.Claims;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
@@ -16,6 +23,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
 import java.util.List;
 
 /**
@@ -32,7 +40,11 @@ public class AppStudentController extends BaseController {
     @Autowired
     private IYunStudentService yunStudentService;
 
+    @Autowired
+    private TokenService tokenService;
 
+    @Autowired
+    private RedisCache redisCache;
     /*
      * @Description :查询当前用户下的学生
      * @Author : 梁少鹏
@@ -70,11 +82,18 @@ public class AppStudentController extends BaseController {
     @ApiImplicitParam(name = "yunStudent", value = "增加学生", dataType = "YunStudent")
     @Log(title = "学生数据", businessType = BusinessType.INSERT)
     @PostMapping("/addStudent")
-    public AjaxResult addStudent(@RequestBody YunStudent yunStudent)
-    {
-        yunStudent.setCreateById(SecurityUtils.getUserId());
-        yunStudent.setCreateBy(SecurityUtils.getUseNickName());
-        return toAjax(yunStudentService.insertYunStudent(yunStudent));
+    public AjaxResult addStudent(YunStudent yunStudent) throws IOException {
+        LoginUser loginUser = tokenService.getLoginUser(ServletUtils.getRequest());
+        if (loginUser!=null)
+        {
+            yunStudent.setCreateById(loginUser.getUser().getUserId());
+            yunStudent.setCreateBy(loginUser.getUser().getNickName());
+            return toAjax(yunStudentService.insertYunStudent(yunStudent));
+        }else{
+            return AjaxResult.error("token无效");
+        }
+
+
     }
     /*
      * @Description :修改学生数据
