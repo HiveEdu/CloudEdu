57a3477896a61a333f40aec9ef806d042e4b76ce
king-sir <1084145369@qq.com>
2020-03-28 21:10:11 +0800

    修改门店接口

---
+++ myedu/src/main/java/com/myedu/app/common/controller/CommonController.java
@@ -0,0 +1,123 @@
+package com.myedu.app.common.controller;
+
+import com.myedu.common.utils.StringUtils;
+import com.myedu.common.utils.file.FileUploadUtils;
+import com.myedu.common.utils.file.FileUtils;
+import com.myedu.framework.config.MyEduConfig;
+import com.myedu.framework.config.ServerConfig;
+import com.myedu.framework.web.domain.AjaxResult;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * 通用请求处理
+ * 
+ * @author ruoyi
+ */
+@Api("APP文件上传下载")
+@RestController
+@RequestMapping("/app")
+public class CommonController
+{
+    private static final Logger log = LoggerFactory.getLogger(CommonController.class);
+
+    @Autowired
+    private ServerConfig serverConfig;
+
+    /**
+     * 通用下载请求
+     * 
+     * @param fileName 文件名称
+     * @param delete 是否删除
+     */
+    @ApiOperation("文件下载")
+    @GetMapping("/common/download")
+    public void fileDownload(String fileName, Boolean delete, HttpServletResponse response, HttpServletRequest request)
+    {
+        try
+        {
+            if (!FileUtils.isValidFilename(fileName))
+            {
+                throw new Exception(StringUtils.format("文件名称({})非法，不允许下载。 ", fileName));
+            }
+            String realFileName = System.currentTimeMillis() + fileName.substring(fileName.indexOf("_") + 1);
+            String filePath = MyEduConfig.getDownloadPath() + fileName;
+
+            response.setCharacterEncoding("utf-8");
+            response.setContentType("multipart/form-data");
+            response.setHeader("Content-Disposition",
+                    "attachment;fileName=" + FileUtils.setFileDownloadHeader(request, realFileName));
+            FileUtils.writeBytes(filePath, response.getOutputStream());
+            if (delete)
+            {
+                FileUtils.deleteFile(filePath);
+            }
+        }
+        catch (Exception e)
+        {
+            log.error("下载文件失败", e);
+        }
+    }
+
+    /**
+     * 通用上传请求
+     */
+    @ApiOperation("单文件文件上传")
+    @PostMapping("/common/upload")
+    public AjaxResult uploadFile(@RequestParam(value = "file", required=false) MultipartFile file) throws Exception
+    {
+        try
+        {
+            // 上传文件路径
+            String filePath = MyEduConfig.getUploadPath();
+            // 上传并返回新文件名称
+            String fileName = FileUploadUtils.upload(filePath, file);
+            String url = serverConfig.getUrl() + fileName;
+            AjaxResult ajax = AjaxResult.success();
+            ajax.put("fileName", fileName);
+            ajax.put("url", url);
+            return ajax;
+        }
+        catch (Exception e)
+        {
+            return AjaxResult.error(e.getMessage());
+        }
+    }
+
+    /**
+     * 通用上传请求
+     */
+    @ApiOperation("多文件文件上传")
+    @PostMapping("/common/uploads")
+    public AjaxResult uploadFiles(@RequestParam(value = "files", required=false) MultipartFile[] files) throws Exception
+    {
+        try
+        {
+            // 上传文件路径
+            String filePath = MyEduConfig.getUploadPath();
+            AjaxResult ajax = AjaxResult.success();
+            String url="";
+            for (int i = 0; i < files.length; i++) {
+                if(files[i]!=null){
+                    String fileName= FileUploadUtils.upload(filePath, files[i]);
+                    url=url+serverConfig.getUrl()+fileName+",";
+                }
+            }
+            ajax.put("url", url);
+            return ajax;
+        }
+        catch (Exception e)
+        {
+            return AjaxResult.error(e.getMessage());
+        }
+    }
+
+}
