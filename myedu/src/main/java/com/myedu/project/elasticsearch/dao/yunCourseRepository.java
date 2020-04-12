4995c39bce540d0c7e790b4f366eb739d38f0227
lianglongfei <1065008257@qq.com>
2020-04-12 19:39:34 +0800

    删除es目录

--- myedu/src/main/java/com/myedu/project/elasticsearch/dao/yunCourseRepository.java
+++
@@ -1,7 +0,0 @@
-package com.myedu.project.elasticsearch.dao;
-
-import com.myedu.project.elasticsearch.entity.yunCourse;
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-
-public interface yunCourseRepository extends ElasticsearchRepository<yunCourse, String> {
-}
