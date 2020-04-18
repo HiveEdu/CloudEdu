01dc47b80dc0c6764b1158652d34ab65a3f8d99e
lianglongfei <1065008257@qq.com>
2020-04-18 17:01:47 +0800

    提交门店端分班管理

---
+++ myedu-ui/src/api/store/class.js
@@ -0,0 +1,53 @@
+import request from '@/utils/request'
+
+// 查询分班管理列表
+export function listClass(query) {
+  return request({
+    url: '/store/class/list',
+    method: 'get',
+    params: query
+  })
+}
+
+// 查询分班管理详细
+export function getClass(id) {
+  return request({
+    url: '/store/class/' + id,
+    method: 'get'
+  })
+}
+
+// 新增分班管理
+export function addClass(data) {
+  return request({
+    url: '/store/class',
+    method: 'post',
+    data: data
+  })
+}
+
+// 修改分班管理
+export function updateClass(data) {
+  return request({
+    url: '/store/class',
+    method: 'put',
+    data: data
+  })
+}
+
+// 删除分班管理
+export function delClass(id) {
+  return request({
+    url: '/store/class/' + id,
+    method: 'delete'
+  })
+}
+
+// 导出分班管理
+export function exportClass(query) {
+  return request({
+    url: '/store/class/export',
+    method: 'get',
+    params: query
+  })
+}
---
+++ myedu-ui/src/views/store/class/index.vue
@@ -0,0 +1,380 @@
+<template>
+  <div class="app-container">
+    <el-form :model="queryParams" ref="queryForm" :inline="true" label-width="68px">
+      <el-form-item label="班级名称" prop="name">
+        <el-input
+          v-model="queryParams.name"
+          placeholder="请输入班级名称"
+          clearable
+          size="small"
+          @keyup.enter.native="handleQuery"
+        />
+      </el-form-item>
+      <el-form-item label="主教老师" prop="bishopTeacher">
+        <el-input
+          v-model="queryParams.bishopTeacher"
+          placeholder="请输入主教老师"
+          clearable
+          size="small"
+          @keyup.enter.native="handleQuery"
+        />
+      </el-form-item>
+      <el-form-item label="助教老师" prop="assistantTeacher">
+        <el-input
+          v-model="queryParams.assistantTeacher"
+          placeholder="请输入助教老师"
+          clearable
+          size="small"
+          @keyup.enter.native="handleQuery"
+        />
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" icon="el-icon-search" size="mini" @click="handleQuery">搜索</el-button>
+        <el-button icon="el-icon-refresh" size="mini" @click="resetQuery">重置</el-button>
+      </el-form-item>
+    </el-form>
+
+    <el-row :gutter="10" class="mb8">
+      <el-col :span="1.5">
+        <el-button
+          type="primary"
+          icon="el-icon-plus"
+          size="mini"
+          @click="handleAdd"
+          v-hasPermi="['store:class:add']"
+        >新增</el-button>
+      </el-col>
+      <el-col :span="1.5">
+        <el-button
+          type="success"
+          icon="el-icon-edit"
+          size="mini"
+          :disabled="single"
+          @click="handleUpdate"
+          v-hasPermi="['store:class:edit']"
+        >修改</el-button>
+      </el-col>
+      <el-col :span="1.5">
+        <el-button
+          type="danger"
+          icon="el-icon-delete"
+          size="mini"
+          :disabled="multiple"
+          @click="handleDelete"
+          v-hasPermi="['store:class:remove']"
+        >删除</el-button>
+      </el-col>
+      <el-col :span="1.5">
+        <el-button
+          type="warning"
+          icon="el-icon-download"
+          size="mini"
+          @click="handleExport"
+          v-hasPermi="['store:class:export']"
+        >导出</el-button>
+      </el-col>
+    </el-row>
+
+    <el-table v-loading="loading" :data="classList" @selection-change="handleSelectionChange">
+      <el-table-column type="selection" width="55" align="center" />
+      <!-- <el-table-column label="班级图标" align="center" prop="classLogo" /> -->
+        <el-table-column ref="table" prop="classLogo" label="Logo" width="60">
+        <template slot-scope="scope">
+          <img  :src="viewImage+'/'+scope.row.classLogo" :onerror="defaultImg" alt="点击打开" class="el-avatar" style="border-radius:10px">
+        </template>
+      </el-table-column>
+      <el-table-column label="班级名称" align="center" prop="name" />
+      <el-table-column label="主教老师" align="center" prop="bishopTeacher" />
+      <el-table-column label="助教老师" align="center" prop="assistantTeacher" />
+      <el-table-column label="备注" align="center" prop="remark" />
+      <el-table-column label="操作" align="center" class-name="small-padding fixed-width">
+        <template slot-scope="scope">
+          <el-button
+            size="mini"
+            type="text"
+            icon="el-icon-edit"
+            @click="handleUpdate(scope.row)"
+            v-hasPermi="['store:class:edit']"
+          >修改</el-button>
+          <el-button
+            size="mini"
+            type="text"
+            icon="el-icon-delete"
+            @click="handleDelete(scope.row)"
+            v-hasPermi="['store:class:remove']"
+          >删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+    
+    <pagination
+      v-show="total>0"
+      :total="total"
+      :page.sync="queryParams.pageNum"
+      :limit.sync="queryParams.pageSize"
+      @pagination="getList"
+    />
+
+    <!-- 添加或修改分班管理对话框 -->
+    <el-dialog :title="title" :visible.sync="open" width="500px">
+      <el-form ref="form" :model="form" :rules="rules" label-width="80px">
+        <el-form-item label="班级名称" prop="name">
+          <el-input v-model="form.name" placeholder="请输入班级名称" />
+        </el-form-item>
+        <el-form-item label="班级图标">
+          <el-upload
+            class="avatar-uploader"
+            ref="upload"
+            :headers="headers"
+            :action="uploadImgUrl"
+            :data="{'type':'stuAvatar'}"
+            :show-file-list="false"
+            :on-success="onSuccess"
+            :on-remove="handleRemove"
+            :on-preview="handlePictureCardPreview"
+            :before-upload="beforeUpload">
+            <img v-if="form.classLogo" :src="viewImage+'/'+form.classLogo" :onerror="defaultImg" class="avatar">
+            <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+          </el-upload>
+        </el-form-item>
+        <el-form-item label="主教老师" prop="bishopTeacher">
+          <el-input v-model="form.bishopTeacher" placeholder="请输入主教老师" />
+        </el-form-item>
+        <el-form-item label="助教老师" prop="assistantTeacher">
+          <el-input v-model="form.assistantTeacher" placeholder="请输入助教老师" />
+        </el-form-item>
+        <el-form-item label="备注" prop="remark">
+          <el-input v-model="form.remark" placeholder="请输入删除标志" />
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button type="primary" @click="submitForm">确 定</el-button>
+        <el-button @click="cancel">取 消</el-button>
+      </div>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+import { listClass, getClass, delClass, addClass, updateClass, exportClass } from "@/api/store/class";
+import { getToken } from '@/utils/auth'
+export default {
+  data() {
+    return {
+      defaultImg: 'this.src="' + require("@/assets/image/deaufalt.jpg") + '"',
+      // 遮罩层
+      loading: true,
+      // 选中数组
+      ids: [],
+      // 非单个禁用
+      single: true,
+      // 非多个禁用
+      multiple: true,
+      // 总条数
+      total: 0,
+      // 分班管理表格数据
+      classList: [],
+      // 弹出层标题
+      title: "",
+      // 是否显示弹出层
+      open: false,
+      // 查询参数
+      queryParams: {
+        pageNum: 1,
+        pageSize: 10,
+        name: undefined,
+        bishopTeacher: undefined,
+        assistantTeacher: undefined,
+        storeId: undefined,
+        createById: undefined,
+        updateById: undefined
+      },
+      // 表单参数
+      form: {},
+      // 表单校验
+      rules: {
+        name: [
+          { required: true, message: "班级名称不能为空", trigger: "blur" }
+        ]
+      },
+      uploadImgUrl: process.env.VUE_APP_BASE_API + "/common/upload", // 上传的图片服务器地址
+      viewImage: process.env.VUE_APP_BASE_API,
+      headers: {
+        Authorization: 'Bearer ' + getToken()
+      },
+      dialogImageUrl: '',
+      dialogVisible: false,
+    };
+  },
+  created() {
+    this.getList();
+  },
+  methods: {
+    /** 查询分班管理列表 */
+    getList() {
+      this.loading = true;
+      listClass(this.queryParams).then(response => {
+        this.classList = response.rows;
+        this.total = response.total;
+        this.loading = false;
+      });
+    },
+    // 取消按钮
+    cancel() {
+      this.open = false;
+      this.reset();
+    },
+    // 表单重置
+    reset() {
+      this.form = {
+        id: undefined,
+        name: undefined,
+        classLogo: undefined,
+        bishopTeacher: undefined,
+        assistantTeacher: undefined,
+        remark: undefined,
+        createBy: undefined,
+        createTime: undefined,
+        updateBy: undefined,
+        updateTime: undefined,
+        delFlag: undefined,
+        storeId: undefined,
+        createById: undefined,
+        updateById: undefined
+      };
+      this.resetForm("form");
+    },
+    /** 搜索按钮操作 */
+    handleQuery() {
+      this.queryParams.pageNum = 1;
+      this.getList();
+    },
+    /** 重置按钮操作 */
+    resetQuery() {
+      this.resetForm("queryForm");
+      this.handleQuery();
+    },
+    // 多选框选中数据
+    handleSelectionChange(selection) {
+      this.ids = selection.map(item => item.id)
+      this.single = selection.length!=1
+      this.multiple = !selection.length
+    },
+    /** 新增按钮操作 */
+    handleAdd() {
+      this.reset();
+      this.open = true;
+      this.title = "添加分班管理";
+    },
+    /** 修改按钮操作 */
+    handleUpdate(row) {
+      this.reset();
+      const id = row.id || this.ids
+      getClass(id).then(response => {
+        this.form = response.data;
+        this.open = true;
+        this.title = "修改分班管理";
+      });
+    },
+    /** 提交按钮 */
+    submitForm: function() {
+      this.$refs["form"].validate(valid => {
+        if (valid) {
+          if (this.form.id != undefined) {
+            updateClass(this.form).then(response => {
+              if (response.code === 200) {
+                this.msgSuccess("修改成功");
+                this.open = false;
+                this.getList();
+              } else {
+                this.msgError(response.msg);
+              }
+            });
+          } else {
+            addClass(this.form).then(response => {
+              if (response.code === 200) {
+                this.msgSuccess("新增成功");
+                this.open = false;
+                this.getList();
+              } else {
+                this.msgError(response.msg);
+              }
+            });
+          }
+        }
+      });
+    },
+    /** 删除按钮操作 */
+    handleDelete(row) {
+      const ids = row.id || this.ids;
+      this.$confirm('是否确认删除分班管理编号为"' + ids + '"的数据项?', "警告", {
+          confirmButtonText: "确定",
+          cancelButtonText: "取消",
+          type: "warning"
+        }).then(function() {
+          return delClass(ids);
+        }).then(() => {
+          this.getList();
+          this.msgSuccess("删除成功");
+        }).catch(function() {});
+    },
+    /** 导出按钮操作 */
+    handleExport() {
+      const queryParams = this.queryParams;
+      this.$confirm('是否确认导出所有分班管理数据项?', "警告", {
+          confirmButtonText: "确定",
+          cancelButtonText: "取消",
+          type: "warning"
+        }).then(function() {
+          return exportClass(queryParams);
+        }).then(response => {
+          this.download(response.msg);
+        }).catch(function() {});
+    },
+     beforeUpload(file){
+    },
+    onSuccess(res,file, fileList){
+      if(res.code=="200"){
+       //this.form.avatar=res.url
+        this.form.classLogo=res.fileName
+        this.msgSuccess("上传成功");
+      }else{
+        this.msgError("上传失败");
+      }
+    },
+    handleRemove(file, fileList) {
+      console.log(file, fileList);
+      this.form.classLogo=null;
+    },
+    handlePictureCardPreview(file) {
+      this.dialogImageUrl = file.url;
+      this.dialogVisible = true;
+    }
+  }
+};
+</script>
+<style>
+  .avatar-uploader .el-upload {
+    border: 1px dashed #d9d9d9;
+    border-radius: 6px;
+    cursor: pointer;
+    position: relative;
+    overflow: hidden;
+  }
+  .avatar-uploader .el-upload:hover {
+    border-color: #409EFF;
+  }
+  .avatar-uploader-icon {
+    font-size: 28px;
+    color: #8c939d;
+    width: 178px;
+    height: 178px;
+    line-height: 178px;
+    text-align: center;
+  }
+  .avatar {
+    width: 178px;
+    height: 178px;
+    display: block;
+  }
+</style>
