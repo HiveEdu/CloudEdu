fd3cd7e85a39455ed48ab82d3543afeb51b842d3
lianglongfei <1065008257@qq.com>
2020-04-18 10:46:37 +0800

    提交附近的门店token校验

--- myedu/src/main/java/com/myedu/app/parents/controller/APPStoreSearchController.java
+++ myedu/src/main/java/com/myedu/app/parents/controller/APPStoreSearchController.java
@@ -1,5 +1,8 @@
 package com.myedu.app.parents.controller;
 
+import com.myedu.common.utils.ServletUtils;
+import com.myedu.framework.security.LoginUser;
+import com.myedu.framework.security.service.TokenService;
 import com.myedu.framework.web.controller.BaseController;
 import com.myedu.framework.web.page.TableDataInfo;
 import com.myedu.project.store.storeSearch.entityVo.StoreSearchVo;
@@ -42,59 +45,68 @@ public class APPStoreSearchController extends BaseController {
     @Autowired
     private ElasticsearchTemplate elasticsearchTemplate;
 
+    @Autowired
+    private TokenService tokenService;
+
     @ApiOperation("附近的门店列表")
     @GetMapping(value = "/findAll")
     public TableDataInfo listProducts(double lon, double lat, String distance,StoreSearchVo storeSearchVo)
     {
-        startPage();
-        //设定搜索半径
-        GeoDistanceQueryBuilder queryBuilder = QueryBuilders.geoDistanceQuery("location")
-                .point(lat, lon)
-                .distance(distance, DistanceUnit.METERS.KILOMETERS)
-                .geoDistance(GeoDistance.PLANE);
-        //按距离排序
-        GeoDistanceSortBuilder sortBuilder = SortBuilders.geoDistanceSort("location", lat, lon);
-        sortBuilder.order(SortOrder.ASC);
-        sortBuilder.point(lat, lon);
-        //构建检索
-        SearchSourceBuilder searchSourceBuilder = SearchSourceBuilder.searchSource()
-                .from(0)
-                .size(20)
-                .query(queryBuilder)
-                .sort(sortBuilder);
-        //构造查询条件
-        NativeSearchQueryBuilder nativeSearchQueryBuilder =
-                new NativeSearchQueryBuilder()
-                        .withFilter(queryBuilder)
-                        .withSort(sortBuilder);
-        Page<StoreSearchVo> page =
-                elasticsearchTemplate.queryForPage(
-                        nativeSearchQueryBuilder.build(), StoreSearchVo.class);
-        List<StoreSearchVo> storeSearchVosNew=new ArrayList<StoreSearchVo>();
-        List<StoreSearchVo> storeSearchVos= page.getContent();
-        storeSearchVos.forEach(storeSearchVo1 -> {
-            double calculate = GeoDistance.ARC.calculate(storeSearchVo1.getLocation().getLat(), storeSearchVo1.getLocation().getLon(), lat, lon, DistanceUnit.METERS);
-            storeSearchVo1.setDistanceMeters(String.valueOf(calculate));
-            List<Long> typeIds=storeSearchVo1.getTypeIds();
-            if(storeSearchVo.getTypeIds()!=null&&storeSearchVo.getTypeIds().size()>0){
-              Long typeId= storeSearchVo.getTypeIds().get(0);
-              Boolean iscontent=false;
-                for (Long typeIdsNew:
-                        typeIds) {
-                    if(typeIdsNew==typeId){
-                        iscontent=true;
-                        break;
+        LoginUser loginUser = tokenService.getLoginUser(ServletUtils.getRequest());
+        if (loginUser!=null) {
+            startPage();
+            //设定搜索半径
+            GeoDistanceQueryBuilder queryBuilder = QueryBuilders.geoDistanceQuery("location")
+                    .point(lat, lon)
+                    .distance(distance, DistanceUnit.METERS.KILOMETERS)
+                    .geoDistance(GeoDistance.PLANE);
+            //按距离排序
+            GeoDistanceSortBuilder sortBuilder = SortBuilders.geoDistanceSort("location", lat, lon);
+            sortBuilder.order(SortOrder.ASC);
+            sortBuilder.point(lat, lon);
+            //构建检索
+            SearchSourceBuilder searchSourceBuilder = SearchSourceBuilder.searchSource()
+                    .from(0)
+                    .size(20)
+                    .query(queryBuilder)
+                    .sort(sortBuilder);
+            //构造查询条件
+            NativeSearchQueryBuilder nativeSearchQueryBuilder =
+                    new NativeSearchQueryBuilder()
+                            .withFilter(queryBuilder)
+                            .withSort(sortBuilder);
+            Page<StoreSearchVo> page =
+                    elasticsearchTemplate.queryForPage(
+                            nativeSearchQueryBuilder.build(), StoreSearchVo.class);
+            List<StoreSearchVo> storeSearchVosNew=new ArrayList<StoreSearchVo>();
+            List<StoreSearchVo> storeSearchVos= page.getContent();
+            storeSearchVos.forEach(storeSearchVo1 -> {
+                double calculate = GeoDistance.ARC.calculate(storeSearchVo1.getLocation().getLat(), storeSearchVo1.getLocation().getLon(), lat, lon, DistanceUnit.METERS);
+                storeSearchVo1.setDistanceMeters(String.valueOf(calculate));
+                List<Long> typeIds=storeSearchVo1.getTypeIds();
+                if(storeSearchVo.getTypeIds()!=null&&storeSearchVo.getTypeIds().size()>0){
+                    Long typeId= storeSearchVo.getTypeIds().get(0);
+                    Boolean iscontent=false;
+                    for (Long typeIdsNew:
+                            typeIds) {
+                        if(typeIdsNew==typeId){
+                            iscontent=true;
+                            break;
+                        }
                     }
+                    if(iscontent){
+                        storeSearchVosNew.add(storeSearchVo1);
+                    }
+                }else{
+                    storeSearchVosNew.add(storeSearchVo1);
                 }
-              if(iscontent){
-                  storeSearchVosNew.add(storeSearchVo1);
-              }
-            }else{
-                  storeSearchVosNew.add(storeSearchVo1);
-            }
-            System.out.println("距离" + (int)calculate + "m");
-        });
-        return getDataTable(storeSearchVosNew);
+                System.out.println("距离" + (int)calculate + "m");
+            });
+            return getDataTable(storeSearchVosNew);
+        }else {
+            return getDataTableLose(null);
+        }
+
 
 
     }
